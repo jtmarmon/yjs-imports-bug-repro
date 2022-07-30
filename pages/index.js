699b731ee281f169a13067b06095c0f7d91ee9be
Jason Marmon <jtmarmon@gmail.com>
2022-07-30 12:29:40 -0400

    minimal repro

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import * as Y from "yjs";
+import {WebsocketProvider} from "y-websocket";
 
 export default function Home() {
   return (
